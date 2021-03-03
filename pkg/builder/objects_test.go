323f1efe15cb1396e46621b4c887e32414c29ccb
Jack Baldry <jack.baldry@grafana.com>
2021-03-03 15:29:16 +0000

    feat: Add FloatType
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- pkg/builder/objects_test.go
+++ pkg/builder/objects_test.go
@@ -9,7 +9,8 @@ func TestObjectPrimitive(t *testing.T) {
 {
   int: 1,
   string: "hello",
-  bool: true
+  bool: true,
+  float: 23.5
 }
 `
 
@@ -17,6 +18,7 @@ func TestObjectPrimitive(t *testing.T) {
 		Int("int", 1),
 		String("string", "hello"),
 		Bool("bool", true),
+		Float("float", 23.5),
 	)
 
 	assertRender(t, o, want)
@@ -25,10 +27,11 @@ func TestObjectPrimitive(t *testing.T) {
 func TestObjectFuncs(t *testing.T) {
 	want := `
 {
-  regular(s="string", b=false, i=3): {
+  regular(s="string", b=false, i=3, f=23.5): {
     s: s,
     b: b,
-    i: i
+    i: i,
+    f: f
   },
   hidden(h=3): h
 }
@@ -40,11 +43,13 @@ func TestObjectFuncs(t *testing.T) {
 				String("s", "string"),
 				Bool("b", false),
 				Int("i", 3),
+				Float("f", 23.5),
 			),
 			Object("",
 				Ref("s", "s"),
 				Ref("b", "b"),
 				Ref("i", "i"),
+				Ref("f", "f"),
 			),
 		),
 		Func("hidden",
